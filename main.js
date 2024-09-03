ffe3fc1e3f49deedbda51379a62e913fd6ad97fc
baxtiyorov2010 <torabaxtiyorov29@gmail.com>
2024-09-03 20:23:17 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+// let text1 = "Hello";
+// let text2 = "World";
+// let text3 = text2.concat("hi", text1);
+// console.log(text3);
+// let text = "5";
+// let padded = text.padStart(4,"0");
+// console.log(text);
+// let text = "5";
+// let padded = text.padEnd(4,"0");
+// console.log(text);
+
+
+
+let a = prompt("ismingizni kriting")
+let s = prompt("mars space loginingizni kiriting")
+let d = prompt("mars space paswordingizni kiriting")
+
+const person = {
+    name: a,
+    login: s,
+    pasword: d,
+};
+console.log(person);
